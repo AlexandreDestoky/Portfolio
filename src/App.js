c6a23993b4893853eb2d2f0c9c465fbc119909d8
Destoky <adestoky@gmail.com>
2021-09-26 22:16:40 +0200

    Debut séparation fichier

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Accueil from './Components/Accueil/Accueil';
 
 function App() {
   return (
     <div className="App">
       <h1>Portfolio</h1>
+      <Accueil/>
+
     </div>
   );
 }
